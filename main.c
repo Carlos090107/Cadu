28dbcf90ba28dbff391524b42bc3572d367cac94
Carlos Eduardo <carlos.amorim2@etec.sp.gov.br>
2023-05-24 17:52:03 -0300

    cadu'

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+main() {
+
+int numero1;
+int numero2;
+
+numero1 = 10;
+numero2 = 20;
+
+printf("o numero 1: %d\nE o numero 2: %d" ,numero1,numero2);
+}
+	
